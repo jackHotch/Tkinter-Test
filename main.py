1817200d03c5b4f2616460a20dda2cc49f16fb9b
jackHotch <jhotchkiss2021@gmail.com>
2025-03-15 21:12:49 -0400

    added text entry

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from tkinter import ttk
 root = tk.Tk()
 root.title("Simple App")
 
-# frame = tk.Frame(root)
+frame = ttk.Frame(root, padding=10)
+frame.grid(row=0, column=0)
 
-entry_btn = ttk.Button(root, text='Hello World')
-entry_btn.grid()
+entry = ttk.Entry(frame)
+entry.grid(row=0, column=0)
+
+entry_btn = ttk.Button(frame, text='Hello World')
+entry_btn.grid(row=0, column=1)
 
 root.mainloop()
